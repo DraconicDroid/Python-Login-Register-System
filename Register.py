c3fb897ebfb8d6dbc9975593390f2e54d51455be
DraconicDroid <74269451+DraconicDroid@users.noreply.github.com>
2022-01-17 14:42:46 +0000

    First Addition #added input and started validation

---
+++ Register.py
@@ -0,0 +1,16 @@
+import tkinter as tk
+
+global Email
+global Username
+global Password
+
+def Validation(): # Validates that the fields the user enters are correct
+
+def input_details():  # Where the user will enter their details
+    global Email
+    global Username
+    global Password
+
+    Email = input("Please Enter your Email")
+    Username = input("Please Enter your Username")
+    Password = input("Please Enter your Password")
