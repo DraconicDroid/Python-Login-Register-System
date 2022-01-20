2246527801298096395d141f77cc55ec4957ffa1
DraconicDroid <diogo452uk@gmail.com>
2022-01-20 16:39:08 +0000

    got register working

--- Login.py
+++ Login.py
@@ -1,5 +1,31 @@
+import tkinter as tk
 import sqlite3
 
-con = sqlite3.connect('UserDatabase.db')
+conn = sqlite3.connect('UserDatabase.db')
+
+curs = conn.cursor()
 
 
+class Login:
+    def __init__(self, emailuser, Password):
+        self.emailuser = emailuser
+        self.password = Password
+
+    def Verification(self):
+        self.emailuser = input("Please enter your Email or Username \n")
+        self.password = input("Please enter your password \n")
+        verificationuser = f"SELECT Email from Records WHERE" \
+                           f" Email='{self.emailuser}'" \
+                           f" OR Username = '{self.emailuser}'" \
+                           f" AND Password = '{self.password}';"
+        curs.execute(verificationuser)
+        if not curs.fetchone():  # An empty result evaluates to False.
+            print("Login failed")
+            self.Verification()
+            return
+        else:
+            print("Welcome")
+
+
+controller = Login("", "")
+controller.Verification()
--- Register.py
+++ Register.py
@@ -3,35 +3,36 @@ import tkinter as tk
 import re
 import sqlite3
 
-conn = sqlite3.connect('UserDatabase.db')
+conn = sqlite3.connect('UserDatabase.db')  # This connects the database to python file
 
-curs = conn.cursor()
+curs = conn.cursor()  # This allows me to interact with the database
 
 
-class Credentials:
-    def __init__(self, Email, Username, Password, Failcheck, Fail):
+class Register:
+    def __init__(self, Email, Username, Password, Failcheck, Fail, Emailyes):
+        # initializes the class with the parameters I specified below
         self.email = Email
         self.username = Username
         self.password = Password
         self.failcheck = Failcheck
         self.fail = Fail
+        self.emailyes = Emailyes
 
-    def Email_Validation(self):  # Validates that the fields the user enters are correct
+    def Email_Validation(self):  # Validates that the email the user entered is correct
         try:
-            # Validate.
+            # Validates the email.
             valid = validate_email(self.email)
 
-            # Update with the normalized form.
-            email = valid.email
             self.fail = 0
         except EmailNotValidError as e:
             # email is not valid, exception message is human-readable
             print(str(e))
-            self.failcheck = True
+            self.failcheck = True  # this occurs when the email is incorrect and enables a loop that is explained below
             self.fail = 1
 
     def Username_Validation(self):
         USERNAME_REGEX = re.compile(r"^[A-Za-z][A-Za-z0-9_]{7,29}$")
+        # Username regex (must be 8 character but less than 28 and cannot include special characters)
         if not USERNAME_REGEX.match(self.username):
             print("Sorry your Username is not in the right format")
             self.failcheck = True
@@ -41,38 +42,77 @@ class Credentials:
 
     def Password_Validation(self):
         PASSWORD_REGEX = re.compile(r"[A-Za-z0-9@#$%^&+=]{8,}")
+        # Password regex (must be 8 characters or more and cannot include special characters)
         if not PASSWORD_REGEX.fullmatch(self.password):
             print("Sorry your Password is not in the right format")
+            print("Your password must be 8 characters or more and cannot include special characters")
             self.failcheck = True
             self.fail = 1
         else:
             self.fail = 0
 
-    def email_database(self):
+    def input_Email(self):
         self.fail = 0
         self.failcheck = False
-        self.email = input("Please Enter your Email\n")
-        self.Email_Validation()
-        if self.failcheck:
-            while self.fail == 1:
-                self.email = input("Please Enter your Email\n")
-                self.Email_Validation()
+        emailQ = input("Would you like to register an email\n")
+        if emailQ in ["Yes", "yes", "Y", "y"]:
+            self.emailyes = True
+            self.email = input("Please Enter your Email\n")
+            self.Email_Validation()
+            if self.failcheck:
+                while self.fail == 1:
+                    self.email = input("Please Enter your Email\n")
+                    self.Email_Validation()
+        elif emailQ in ["No", "no", "N", "n"]:
+            self.emailyes = False
+            print("Your email won't be registered")
+        else:
+            print("Invalid input: you must enter yes or no")
+            self.input_Email()
+            return
+        self.input_Username()
+
+    def input_Username(self):
         self.failcheck = False
+        print("")
+        print("")
+        print("Your Username must be 8 characters but less than 29 and cannot include special characters")
         self.username = input("Please Enter your Username\n")
         self.Username_Validation()
         if self.failcheck:
             while self.fail == 1:
+                print("Your Username must be 8 characters but less than 29 and cannot include special characters")
                 self.username = input("Please Enter your Username\n")
                 self.Username_Validation()
+        self.input_Password()
+
+    def input_Password(self):
         self.failcheck = False
+        print("")
+        print("")
+        print("Your password must be 8 characters or more and cannot include special characters")
         self.password = input("Please Enter your Password\n")
         self.Password_Validation()
         if self.failcheck:
             while self.fail == 1:
+                print("Your password must be 8 characters or more and cannot include special characters")
                 self.password = input("Please Enter your Password\n")
                 self.Password_Validation()
+        self.Credentials_database()
+
+    def Credentials_database(self):
         print(self.email, self.username, self.password)
+        if self.emailyes:
+            curs.execute("INSERT INTO Records (Email, Username, Password) VALUES (?, ?, ?)",
+                         (self.email, self.username, self.password))
+            conn.commit()
+            conn.close()
+        if not self.emailyes:
+            curs.execute("INSERT INTO Records (Username, Password) VALUES (?, ?)",
+                         (self.username, self.password))
+            conn.commit()
+            conn.close()
 
 
-register = Credentials("", "", "", False, 0)
-register.email_database()
+controller = Register("", "", "", False, 0, True)
+controller.input_Email()
